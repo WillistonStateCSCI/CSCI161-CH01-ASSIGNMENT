9dab6b700a784998efe7a6370ec8e1f65cb5fe85
Amanda Skinner <Amanda.Skinner.1@willistonstate.edu>
2026-08-26 20:29:02 -0500

    Remove divBy2 test case for input 1024
    Removed test case for divBy2 with input 1024.

--- .github/tests/Grader_1_19.java
+++ .github/tests/Grader_1_19.java
@@ -8,7 +8,6 @@ public class Grader_1_19 {
         check("divBy2(26)", C1_19.divBy2(26), 4);
         check("divBy2(2)", C1_19.divBy2(2), 1);
         check("divBy2(1)", C1_19.divBy2(1), 0);
-        check("divBy2(1024)", C1_19.divBy2(1024), 9);
         System.out.println(pass + " passed, " + fail + " failed");
         if (fail > 0) System.exit(1);
     }
